8d8726f4115662f1e9664f45a52b222735610e49
Lola <lola_chacon8@hotmail.com>
2024-10-31 15:41:49 +0100

    ejemplo de conexion mysql y correcion en package.json

--- src/config/db.js
+++ src/config/db.js
@@ -1,2 +1,15 @@
 //aqui esta la base de datos mysql requerida
 const mysql = require ('mysql2')
+
+const pool = mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'',
+    port:1234,
+    database:'nombreBaseDatos'
+})
+
+
+
+
+module.exports = pool.promise();
