25c6cd701ff811737a7705479136df79a9583341
fran <fran10.jimenezromera@gmail.com>
2024-11-30 18:39:52 +0100

    Create Delivery
    Se ha solucionado la asincronicidad de las promesas del create Delivery

--- src/controllers/operator.controller.js
+++ src/controllers/operator.controller.js
@@ -1,4 +1,4 @@
-const { selectAll, selectById, postDelivery, postProducts, selectProductByDelivery} = require("../models/operator.model");
+const { selectAll, selectById, postDelivery, postProducts, selectProductByDelivery, updateById} = require("../models/operator.model");
 
 const getAllDeliveryByUser = async (req, res, next) => {
     console.log(req.user)
@@ -25,7 +25,11 @@ const createDelivery = async (req, res, next) => {
     try {
         const [newDelivery] = await postDelivery (req.body);
         const [result] = await selectById(newDelivery.insertId)
-        result.products = await postProducts (req.body.products, newDelivery.insertId)
+        for (const producto of req.body.products) {
+            await postProducts(producto, newDelivery.insertId);
+          }
+        const [productos] = await selectProductByDelivery(newDelivery.insertId)
+        result[0].productos = productos
         res.json(result);
     } catch (error) {
         next(error)
@@ -35,7 +39,8 @@ const updateDeliveryById = async (req, res, next) => {
     const {id_delivery} = req.params;
     try {
         const [result] = await updateById(req.body, id_delivery);
-        result.products = await updateProducts (req.body.products, id_delivery)
+        console.log(result)
+        result[0].products = await updateProducts (req.body.products, id_delivery)
         res.json(result);
     } catch (error) {
         next(error)
--- src/models/operator.model.js
+++ src/models/operator.model.js
@@ -3,7 +3,7 @@ const pool = require("../config/db");
 function selectAll(userId){
     console.log(userId);
     return pool.query(
-        'SELECT u.id_user, u.name, u.surname, d.send_date, d.origin_warehouse_id, d.destination_warehouse_id, t.plate, d.status FROM user u INNER JOIN truck t ON u.id_user = t.driver_id_user INNER JOIN delivery d ON t.driver_id_user = d.truck_id_truck WHERE u.id_user = ?;', 
+        'SELECT d.id_delivery, u.id_user, u.name, u.surname, d.send_date, d.origin_warehouse_id, d.destination_warehouse_id, t.plate, d.status FROM user u INNER JOIN truck t ON u.id_user = t.driver_id_user INNER JOIN delivery d ON t.driver_id_user = d.truck_id_truck WHERE u.id_user = ?;', 
         [userId]
     );
 }
@@ -19,14 +19,10 @@ function postDelivery(data){
         [data.send_date, data.recive_date,data.truck_id_truck, data.origin_warehouse_id, data.destination_warehouse_id, data.status, data.comments])
 }
 function postProducts(products, id_delivery){
-    console.log(id_delivery)
     const delivery = id_delivery
-    products.forEach(product => {
-        pool.query('INSERT INTO delivery_products (product_id_product, quantity, delivery_id_delivery) VALUES (?, ?, ?)', [product.product_id, product.quantity, delivery])
-    });
-    return pool.query('SELECT * FROM delivery_produts WHERE delivery_id_delivery = ?', [delivery])
+    return pool.query('INSERT INTO delivery_products (product_id_product, quantity, delivery_id_delivery) VALUES (?, ?, ?)', [products.product_id, products.quantity, delivery])
 }
-function updateDeliveryById(delivery_info, id_delivery){
+function updateById(delivery_info, id_delivery){
     return pool.query('UPDATE delivery SET (send_date = ?, received_date = ?, truck_id_truck = ?, origin_warehouse_id = ?, destination_warehouse_id = ?, status = ?, comments = ?) WHERE id_delivery = ?', 
         [delivery_info.send_date, delivery_info.recive_date, delivery_info.truck_id_truck, delivery_info.origin_warehouse_id, delivery_info.destination_warehouse_id, delivery_info.status, delivery_info.comments, id_delivery])
 }
@@ -34,9 +30,9 @@ function updateProductsById(products_info, id_delivery){
     const [products] = pool.query('DELETE delivery_products WHERE id_delivery = ?', [id_delivery])
     products_info.forEach(product => {
         pool.query('INSERT INTO delivery_products (product_id_product, quantity, delivery_id_delivery) VALUES (?, ?, ?)', [product.product_id, product.quantity, id_delivery])
-    });
+    })
     return pool.query('SELECT * FROM delivery_produts WHERE delivery_id_delivery = ?', [id_delivery])
 }
 module.exports = {
-    selectAll, selectById, postDelivery, postProducts, updateDeliveryById, updateProductsById, selectProductByDelivery
+    selectAll, selectById, postDelivery, postProducts, updateById, updateProductsById, selectProductByDelivery
 }
