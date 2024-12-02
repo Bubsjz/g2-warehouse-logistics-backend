b2c99f8a77172a08cbe116661723702b6c108551
Berard <berard_65@hotmail.com>
2024-12-02 18:14:20 +0100

    Controlador verifyIncomingOrder

--- src/controllers/manager.controller.js
+++ src/controllers/manager.controller.js
@@ -1,4 +1,4 @@
-const { selectOutgoingOrders, selectIncomingOrders, changeOrderStatus, selectOrderById, selectProductsById } = require("../models/manager.model")
+const { selectOutgoingOrders, selectIncomingOrders, changeOrderStatus, selectOrderById, selectProductsById, selectOutgoingOrderById, selectIncomingOrderById } = require("../models/manager.model")
 const { selectById } = require("../models/operator.model")
 
 // Outgoing deliveries
@@ -27,9 +27,6 @@ const getIncomingOrders = async (req, res, next) => {
         }
         return order
       })
-
-
-
       res.json(transformedOrders)
 
     } catch (error) {
@@ -43,16 +40,17 @@ const getOrderById = async (req, res, next) => {
     try {
         const [order] = await selectOrderById(orderId)
         const [products] = await selectProductsById(orderId)
-        console.log(products)
+        // console.log(products)
         order[0].products = products
         res.json(order)
+
     } catch (error) {
         next(error)
     }
 }
 
-// Status update
-const updateOrderStatus = async (req, res, next) => {
+// Status update outgoing deliveries
+const updateOutgoingOrder = async (req, res, next) => {
     const orderId = req.params.id
     const { status, comments = null } = req.body
 
@@ -66,7 +64,28 @@ const updateOrderStatus = async (req, res, next) => {
         }
 
         await changeOrderStatus(orderId, status, comments)
-        res.json({ message: "Order status updated successfully", status })
+
+        const [updatedOrder] = await selectOutgoingOrderById(orderId)
+        res.json({ message: "Order status updated successfully", updatedOrder })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+// Incoming order verification
+const verifyIncomingOrder = async (req, res, next) => {
+    const orderId = req.params.id
+    const { status, comments } = req.body
+    console.log(status)
+    try {
+        if (!["approved", "not approved"].includes(status)) {
+            return res.status(400).json({ message: "Invalid status" })
+        }
+        await changeOrderStatus(orderId, status, comments)
+
+        const [verifiedOrder] = await selectIncomingOrderById(orderId)
+        res.json({ message: "Order status updated successfully", verifiedOrder })
 
     } catch (error) {
         next(error)
@@ -74,5 +93,5 @@ const updateOrderStatus = async (req, res, next) => {
 }
 
 module.exports = {
-    getOutgoingOrders, getIncomingOrders, updateOrderStatus, getOrderById
+    getOutgoingOrders, getIncomingOrders, updateOutgoingOrder, getOrderById, verifyIncomingOrder
 }
--- src/routes/api/manager.routes.js
+++ src/routes/api/manager.routes.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 
-const { getOutgoingOrders, getIncomingOrders, updateOrderStatus, getOrderById } = require('../../controllers/manager.controller');
+const { getOutgoingOrders, getIncomingOrders, getOrderById, updateOutgoingOrder, verifyIncomingOrder } = require('../../controllers/manager.controller');
 
 
 router.get("/outgoing-orders", getOutgoingOrders);
 router.get("/incoming-orders", getIncomingOrders)
 router.get("/review-order/:id", getOrderById)
 
-router.put("/review-order/:id", updateOrderStatus)
+router.put("/review-order/:id", updateOutgoingOrder)
+router.put("/verify-order/:id", verifyIncomingOrder)
 
 
 module.exports = router;
