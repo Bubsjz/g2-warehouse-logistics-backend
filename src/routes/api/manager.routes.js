8723ac96720185eeb943310a19451d2d5f046409
Berard <berard_65@hotmail.com>
2024-12-05 13:27:26 +0100

    Testing crud manager con front, añadidos middlewares
    validateDeliveryOwnership & validateWarehouseId

--- src/routes/api/manager.routes.js
+++ src/routes/api/manager.routes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 
 const { getOutgoingOrders, getIncomingOrders, getOrderById, updateOutgoingOrder, verifyIncomingOrder } = require('../../controllers/manager.controller');
+const { validateDeliveryOwnership } = require('../../utils/middlewares');
 
 
 router.get("/outgoing-orders", getOutgoingOrders);
 router.get("/incoming-orders", getIncomingOrders)
-router.get("/review-order/:id", getOrderById)
+router.get("/review-order/:id", validateDeliveryOwnership, getOrderById)
 
-router.put("/review-order/:id", updateOutgoingOrder)
-router.put("/verify-order/:id", verifyIncomingOrder)
+router.put("/review-order/:id", validateDeliveryOwnership, updateOutgoingOrder)
+router.put("/verify-order/:id", validateDeliveryOwnership, verifyIncomingOrder)
 
 
 module.exports = router;
