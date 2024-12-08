c66a8fcb7020538a49e94f55fe7431236656cceb
Rodrigo <rofraiesjob@gmail.com>
2024-12-08 13:04:52 +0100

    Ajustes problemas fecha recepción

--- src/models/operator.model.js
+++ src/models/operator.model.js
@@ -8,7 +8,7 @@ function selectAll(userId){
     );
 }
 function selectById(id_delivery){
-    return pool.query ('SELECT d.id_delivery AS id_delivery, d.send_date AS send_date, wS.name AS origin_warehouse_name, wS.locality AS origin_warehouse_locality, wD.name AS destination_warehouse_name, wD.locality AS destination_warehouse_locality, d.status AS status, t.plate AS plate, d.comments AS comments FROM delivery d INNER JOIN warehouse wS ON d.origin_warehouse_id = wS.id_warehouse INNER JOIN warehouse wD ON d.destination_warehouse_id = wD.id_warehouse INNER JOIN truck t ON t.id_truck = d.truck_id_truck WHERE d.id_delivery = ?;'
+    return pool.query ('SELECT d.id_delivery AS id_delivery, d.send_date AS send_date, d.received_date AS received_date, wS.name AS origin_warehouse_name, wS.locality AS origin_warehouse_locality, wD.name AS destination_warehouse_name, wD.locality AS destination_warehouse_locality, d.status AS status, t.plate AS plate, d.comments AS comments FROM delivery d INNER JOIN warehouse wS ON d.origin_warehouse_id = wS.id_warehouse INNER JOIN warehouse wD ON d.destination_warehouse_id = wD.id_warehouse INNER JOIN truck t ON t.id_truck = d.truck_id_truck WHERE d.id_delivery = ?;'
 , [id_delivery]);
 }
 function selectProductByDelivery(id_delivery){
@@ -16,7 +16,7 @@ function selectProductByDelivery(id_delivery){
 }
 function postDelivery(data){
     return pool.query ('INSERT INTO delivery (send_date, received_date, truck_id_truck, origin_warehouse_id, destination_warehouse_id, status, comments) VALUES (?, ?, (SELECT id_truck FROM truck WHERE plate = ?), (SELECT id_warehouse FROM warehouse WHERE name = ?), (SELECT id_warehouse FROM warehouse WHERE name = ?), ?, ?)',
-        [data.send_date, data.recive_date,data.plate, data.origin_warehouse_name, data.destination_warehouse_name, data.status, data.comments])
+        [data.send_date, data.received_date,data.plate, data.origin_warehouse_name, data.destination_warehouse_name, data.status, data.comments])
 }
 function postProducts(products, id_delivery){
     const delivery = id_delivery
@@ -24,7 +24,7 @@ function postProducts(products, id_delivery){
 }
 function updateById(delivery_info, id_delivery){
     return pool.query('UPDATE delivery SET send_date = ?, received_date = ?, truck_id_truck = (SELECT id_truck FROM truck WHERE plate = ?), origin_warehouse_id = (SELECT id_warehouse FROM warehouse WHERE name = ?), destination_warehouse_id = (SELECT id_warehouse FROM warehouse WHERE name = ?), status = ?, comments = ? WHERE id_delivery = ?;', 
-        [delivery_info.send_date, delivery_info.recive_date, delivery_info.plate, delivery_info.origin_warehouse_name, delivery_info.destination_warehouse_name, delivery_info.status, delivery_info.comments, id_delivery])
+        [delivery_info.send_date, delivery_info.received_date, delivery_info.plate, delivery_info.origin_warehouse_name, delivery_info.destination_warehouse_name, delivery_info.status, delivery_info.comments, id_delivery])
 }
 function updateProductsById(products_info, id_delivery){
     return pool.query('INSERT INTO delivery_products (product_id_product, quantity, delivery_id_delivery) VALUES (?, ?, ?)',
